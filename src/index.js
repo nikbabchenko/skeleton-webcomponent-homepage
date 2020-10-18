22b9fe0c021b130bdd1b4266a91db28379946400
Mykola Babchenko <mykolababchenko@truckstop.com>
2020-10-18 18:25:43 +0300

    Adds main page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import './styles.css';
+import "prism-themes/themes/prism-xonokai.css";
+import "prismjs";
+import "prismjs/plugins/unescaped-markup/prism-unescaped-markup.min.js";
+import "prismjs/plugins/toolbar/prism-toolbar.css";
+import "prismjs/plugins/toolbar/prism-toolbar.min.js";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js";
+import "./styles.scss";
 
-import {defineCustomElements as initSkeleton} from 'skeleton-webcomponent-loader/loader/index.js';
+import { defineCustomElements as initSkeleton } from "skeleton-webcomponent-loader/loader/index.js";
 
 initSkeleton();
