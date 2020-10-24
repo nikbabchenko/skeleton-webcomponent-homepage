15d773fe4c29f7775668b52714ea455cf3154173
Mykola Babchenko <mykolababchenko@truckstop.com>
2020-10-25 00:29:50 +0300

    Fixes one of the snippets

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Skeleton Loader Demo',
-      template: 'index.html'
+      title: "Skeleton Loader Demo",
+      template: "index.html",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[fullhash].css',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].[fullhash].css'
+      filename: isDevelopment ? "[name].css" : "[name].[fullhash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[fullhash].css",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: 'assets', to: '.' },
-      ],
+      patterns: [{ from: "assets", to: "." }],
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
@@ -34,26 +32,26 @@ module.exports = {
         test: /\.css$/i,
         use: [
           // Creates `style` nodes from JS strings
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
         ],
       },
       {
         test: /\.s(a|c)ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
         ],
-      }
+      },
     ],
   },
   devServer: {
-    contentBase: './docs',
-    open: true
+    contentBase: "./docs",
+    open: true,
   },
 };
